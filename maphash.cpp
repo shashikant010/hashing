b7130dbd7d0051145b7293f7cf7d5d1a9d349c82
shashikant010 <shashikantyadav0503@gmail.com>
2024-02-03 13:57:18 +0530

    hashing using map

---
+++ maphash.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+int n;
+cout<<"enter the size of array you want ";
+cin >>n;
+int arr[n];
+cout<<"enter the elements of array ";
+for (int i = 0; i < n; i++)
+{
+    cin>>arr[i];
+}
+
+map <int,int> mpp;
+
+for (int i = 0; i < n; i++)
+{
+    mpp[arr[i]]++;
+}
+
+cout<<"enter number you want to search ";
+int key;
+cin>>key;
+cout<<"the "<<key<<" is repeated "<<mpp[key]<<" times";
+
+
+
+
+return 0;
+}
